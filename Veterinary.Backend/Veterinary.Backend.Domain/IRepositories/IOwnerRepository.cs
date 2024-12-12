5eb0743297172359a744c96349283a0977c96847
ManuelTello <manuelTello.business@gmail.com>
2024-12-12 14:49:59 -0300

    Owner repository and interface added

--- Veterinary.Backend/Veterinary.Backend.Domain/IRepositories/IOwnerRepository.cs
+++ Veterinary.Backend/Veterinary.Backend.Domain/IRepositories/IOwnerRepository.cs
@@ -1,8 +1,12 @@
+using Veterinary.Backend.Domain.AggregateRoots.Owner;
+
 namespace Veterinary.Backend.Domain.IRepositories
 {
     public interface IOwnerRepository
     {
+        public Task SaveChangesAsync();
         
+        public Task<Guid> Save(Owner owner, CancellationToken cancellationToken);
     }
 }
 
--- Veterinary.Backend/Veterinary.Backend.Infrastructure/Persistence/Repositories/OwnerRepository.cs
+++ Veterinary.Backend/Veterinary.Backend.Infrastructure/Persistence/Repositories/OwnerRepository.cs
@@ -1,10 +1,28 @@
+using Veterinary.Backend.Domain.AggregateRoots.Owner;
 using Veterinary.Backend.Domain.IRepositories;
+using Veterinary.Backend.Infrastructure.Persistence.Context;
 
 namespace Veterinary.Backend.Infrastructure.Persistence.Repositories
 {
     public class OwnerRepository:IOwnerRepository
     {
+        private readonly VeterinaryContext _context;
+
+        public OwnerRepository(VeterinaryContext context)
+        {
+            this._context = context; 
+        }
         
+        public async Task SaveChangesAsync()
+        {
+            await this._context.SaveChangesAsync(); 
+        }
+
+        public async Task<Guid> Save(Owner owner, CancellationToken cancellationToken)
+        {
+            await this._context.Owners.AddAsync(owner);
+            return owner.Id;
+        }
     }
 }
 
